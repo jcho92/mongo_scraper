cb286d5ba662325f58a41fd4559d763fc73bb2a2
jcho <john92cho@hotmail.ca>
2019-02-13 08:31:32 -0500

    updates

--- public/js/app.js
+++ public/js/app.js
@@ -2,118 +2,97 @@
 
 $(document).on("click", ".save", function () {
 
-    var id = $(this).data("id")
-    var text = $("#" + id).text()
-    var anchor = $("#" + id).children().attr("href")
-   
-    $.ajax({
-        method: "POST",
-        url: "/update/",
-        data: {
-            title: text,
-            link: anchor,
-            id: id
-        }
-    }).then(function (data) {
-        console.log(data)
-    })
+  var id = $(this).data("id")
+  var text = $("#" + id).text()
+  var anchor = $("#" + id).children().attr("href")
+
+  $.ajax({
+    method: "POST",
+    url: "/update/",
+    data: {
+      title: text,
+      link: anchor,
+      id: id
+    }
+  }).then(function (data) {
+    console.log(data)
+  })
 
 
 })
 
 $(document).on("click", ".addNote", function () {
-
-    var id = $(this).data("id")
-    var text = $("#" + id).text()
-    var anchor = $("#" + id).children().attr("href")
-
-    $.ajax({
-        method:"GET",
-        url: "/notes/" + id,
-        data: {
-            id: id
-        }
-    }).then(function(noteData){
-        
-        for (var i = 0 ; i < noteData.length; i++ ){
-            console.log(noteData[i].body);  
-            $("#savedNotes").append("<p data-id='" + noteData[i]._id+ "'>" + noteData[i].body + "</p>");
-            $("#savedNotes").append("<button class='note-delete' data-id=" + noteData[i]._id + ">delete</button>")
-        }
-        
-        
-    })
-   
-    console.log(id)
-    $.ajax({
-        method: "GET",
-        url: "/article/" +id,
-        data: {
-            title: text,
-            link: anchor,
-            id: id
-        }
-    }).then(function (data) {
-        $("#modal").css("display", "block");
-        $("#note-title").html(data[0].title);
-        $("#note-title").attr("data-id", data[0].id);
-    })
-})
-
-$(document).on("click", "#note-button", function(){
-    var dataid = $("#note-title").attr("data-id")
-    var inputData = $("#note-content").val()
-    var titleData = $("#note-title").text()
-    console.log(dataid)
-    console.log(titleData)
-    console.log(inputData)
+  var id = $(this).data("id")
+  var text = $("#" + id).text()
+  var anchor = $("#" + id).children().attr("href")
+  $.ajax({
+    method: "GET",
+    url: "/notes/" + id,
+    data: {
+      id: id
+    }
+  }).then(function (noteData) {
+    for (var i = 0; i < noteData.length; i++) {
+      console.log(noteData[i].body);
       
-        
-        $.ajax({
-          method: "POST",
-          url: "/article/" + dataid,
-          data: {
-           
-            title: titleData,
-            id: dataid,
-            body:  inputData
-          }
-        })
-          // With that done
-          .then(function(data) {
-            // Log the response
-            console.log(data);
-            // Empty the notes section
-        
-          });
-      
-        // Also, remove the values entered in the input and textarea for note entry
-       
+      $("#savedNotes").append("<p data-id='" + noteData[i]._id + "'>" + noteData[i].body + "</p>");
+      $("#savedNotes").append("<button class='note-delete' data-id=" + noteData[i]._id + ">delete</button>")
+    }
+  })
+  $.ajax({
+    method: "GET",
+    url: "/article/" + id,
+    data: {
+      title: text,
+      link: anchor,
+      id: id
+    }
+  }).then(function (data) {
+    $("#modal").css("display", "block");
+    $("#note-title").html(data[0].title);
+    $("#note-title").attr("data-id", data[0].id);
+  })
 })
 
 
-$(document).on("click", ".note-delete", function(){
-    var dataid = $(this).data("id")
-    console.log(dataid)
-        $.ajax({
-          method: "GET",
-          url: "/delete/" + dataid,
-          data: {
-           
-           
-            id: dataid,
-          }
-        })
-          // With that done
-          .then(function(data) {
-            // Log the response
-            console.log(data);
-            // Empty the notes section
-           
-          });
-      
-        // Also, remove the values entered in the input and textarea for note entry
-       
+$(document).on("click", "#note-button", function () {
+  var dataid = $("#note-title").attr("data-id")
+  var inputData = $("#note-content").val()
+  var titleData = $("#note-title").text()
+  $.ajax({
+    method: "POST",
+    url: "/article/" + dataid,
+    data: {
+      title: titleData,
+      id: dataid,
+      body: inputData
+    }
+  })
+    .then(function (data) {
+    alert("succes")
+    location.reload();
+    });
 })
 
 
+$(document).on("click", ".note-delete", function () {
+  var dataid = $(this).data("id")
+  console.log(dataid)
+  $.ajax({
+    type: 'DELETE',
+    url: "/delete/" + dataid,
+    data: {
+      id: dataid,
+    }
+  })
+    .then(function (data) {
+    alert("success")
+    location.reload()
+    
+    });
+})
+
+$(document).on("click", ".close", function(){
+  location.reload()
+
+})
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,38 +6,25 @@ var db = require("../models");
 module.exports = function (app) {
 
     app.get("/", function (req, res) {
-        // db.Article.remove({})
-        //     .then(function () {
-        //         console.log("removed database")
-        //     })
-
-        // First, we grab the body of the html with axios
         axios.get("https://www.echojs.com/").then(function (response) {
-            // Then, we load that into cheerio and save it to $ for a shorthand selector
+            // T we load that into cheerio and save it to $ for a shorthand selector
             var $ = cheerio.load(response.data);
             var result = [];
             console.log($.children)
             // Now, we grab every h2 within an article tag, and do the following:
             $("article").each(function (i, element) {
-                // Save an empty result object
-                console.log($(this).children("h2").text())
-                console.log($(this).attr("data-news-id"))
-
                 var data = {
                     title: $(this).children("h2").text(),
                     link: $(this).children("h2").children("a").attr("href"),
                     id: $(this).attr("data-news-id")
                 }
+                // Save an empty result object
                 result.push(data)
             });
             res.render("index", {
                 story: result
             });
-
-
         });
-        // Send a message to the client
-
     });
     // api to grab all the articles 
     app.post("/update", function (req, res) {
@@ -52,10 +39,8 @@ module.exports = function (app) {
                             console.log(dbArticle)
                         })
                         .catch(function (err) {
-                            // If an error occurred, send it to the client
                             res.json(err);
-                        });
-
+                        })
                 }
                 else {
                     console.log("already exists")
@@ -73,7 +58,6 @@ module.exports = function (app) {
                 res.render("saved", {
                     data: dbdata
                 })
-                console.log(dbdata)
             }).catch(function (err) {
                 // If an error occurred, send it to the client
                 res.json(err);
@@ -95,9 +79,6 @@ module.exports = function (app) {
                 console.log(dbNote)
                 // console.log(dbNote.body)
                 console.log(req.params.id)
-                // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-                // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-                // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
                 return db.Article.findOneAndUpdate({ id: req.params.id }, { note: dbNote.id }, { new: true });
             })
             .then(function (dbArticle) {
@@ -114,13 +95,13 @@ module.exports = function (app) {
     app.get("/notes", function (req, res) {
         db.Note.find({})
             .then(function (dbNote) {
-               res.json(dbNote)
+                res.json(dbNote)
             })
     })
     // finds a specific note based on the id
-    app.get("/notes/:id", function(req, res){
-        db.Note.find({id:req.params.id})
-            .then(function(dbNote){
+    app.get("/notes/:id", function (req, res) {
+        db.Note.find({ id: req.params.id })
+            .then(function (dbNote) {
                 res.send(dbNote)
             })
     })
@@ -133,19 +114,19 @@ module.exports = function (app) {
             })
     })
 
-    app.get("/clear", function (req, res) {
+    app.delete("/clear", function (req, res) {
         db.Article.remove({})
             .then(function (dbdata) {
                 console.log(dbdata)
             })
     })
 
-    app.get("/delete/:id", function(req, res){
-        db.Note.findOneAndDelete({_id:req.params.id})
-        .then(function(noteData){
-           res.json(noteData)
+    app.delete("/delete/:id", function (req, res) {
+        db.Note.findOneAndDelete({ _id: req.params.id })
+            .then(function (noteData) {
+                res.json(noteData)
 
-        })
+            })
     })
 
 
